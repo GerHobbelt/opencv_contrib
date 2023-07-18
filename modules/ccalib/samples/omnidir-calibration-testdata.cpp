e3cb95613d36326257bc3e6db68e802af8b14549
Stefan Spiss <stefan.spiss@student.uibk.ac.at>
2023-07-18 13:35:58 +0200

    Adapted executable to generate test data for omni cam model to allow setting the scaling and rotation parameters for undistortion and rectification.

--- modules/ccalib/samples/omnidir-calibration-testdata.cpp
+++ modules/ccalib/samples/omnidir-calibration-testdata.cpp
@@ -4,8 +4,34 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
-int main(void)
+int main(int argc, char** argv)
 {
+    const std::string keys = "{help h usage ?||print this message}"
+                                 "{scale0 |0.0| scaling parameter for estimating new camera instrinsics for undistort rectify}"
+                                 "{scale1 |1.0| scaling parameter for estimating new camera instrinsics for undistort rectify}"
+                                 "{raxis |""| rotation axis for estimating new camera instrinsics for undistort rectify}"
+                                 "{rangmul |0.0| used by multiplying pi with multiplier to get rotation angle used for estimating new camera instrinsics for undistort rectify}"
+                                 "{help||show help}";
+
+    cv::CommandLineParser parser(argc, argv, keys);
+    if (parser.has("help"))
+    {
+        parser.printMessage();
+        return 0;
+    }
+
+    double scale0 = parser.get<double>("scale0");
+    double scale1 = parser.get<double>("scale1");
+    std::string rotAx = parser.get<std::string>("raxis");
+    double rotAngMul = parser.get<double>("rangmul");
+
+    if (!parser.check())
+    {
+        parser.printMessage();
+        parser.printErrors();
+        return -1;
+    }
+
     cv::Size imageSize(1280, 800);
     const int n_images = 34;
     const std::string datasetRepositoryPath = "/home/stefan/Documents/DifferentProjects/OpenCVPullRequests/opencv_extra/testdata/cv/cameracalibration/fisheye";
@@ -62,21 +88,24 @@ int main(void)
     double rmsf = cv::fisheye::calibrate(objectPoints, imagePoints, imageSize, Kf, Df, cv::noArray(), cv::noArray(), flagf, cv::TermCriteria(3, 20, 1e-6));
     std::cout << "RMS of calibration of fisheye: " << rmsf << std::endl;
 
-    cv::Mat E = cv::Mat::eye(3, 3, cv::DataType<double>::type);
-    /* cv::Mat R_xp; */
-    /* cv::Rodrigues(cv::Vec3d(CV_PI*0.125, 0, 0), E); */
-    /* cv::Rodrigues(cv::Vec3d(0, CV_PI*0.125, 0), E); */
-    /* cv::Rodrigues(cv::Vec3d(0, 0, CV_PI*0.125), E); */
+    cv::Mat R = cv::Mat::eye(3, 3, cv::DataType<double>::type);
+    double rotAng = CV_PI * rotAngMul;
+
+    if (rotAx == "x") {
+        cv::Rodrigues(cv::Vec3d(rotAng, 0, 0), R);
+    } else if (rotAx == "y") {
+        cv::Rodrigues(cv::Vec3d(0, rotAng, 0), R);
+    } else if (rotAx == "z") {
+        cv::Rodrigues(cv::Vec3d(0, 0, rotAng), R);
+    }
+
     cv::Mat K_new_p(3, 3, cv::DataType<double>::type);
     cv::Mat K_new_l(3, 3, cv::DataType<double>::type);
 
-    double scale1 = 1.5;
-    double scale2 = 1.5;
-
-    cv::omnidir::estimateNewCameraMatrixForUndistortRectify(K, D, xi, imageSize, E, K_new_p, cv::omnidir::RECTIFY_PERSPECTIVE, scale1, scale2);
-    cv::omnidir::estimateNewCameraMatrixForUndistortRectify(K, D, xi, imageSize, E, K_new_l, cv::omnidir::RECTIFY_LONGLATI, scale1, scale2);
+    cv::omnidir::estimateNewCameraMatrixForUndistortRectify(K, D, xi, imageSize, R, K_new_p, cv::omnidir::RECTIFY_PERSPECTIVE, scale0, scale1);
+    cv::omnidir::estimateNewCameraMatrixForUndistortRectify(K, D, xi, imageSize, R, K_new_l, cv::omnidir::RECTIFY_LONGLATI, scale0, scale1);
     cv::Mat K_new_f;
-    cv::fisheye::estimateNewCameraMatrixForUndistortRectify(K, D, imageSize, E, K_new_f, scale1, imageSize, scale2);
+    cv::fisheye::estimateNewCameraMatrixForUndistortRectify(K, D, imageSize, R, K_new_f, scale0, imageSize, scale1);
 
     std::cout << "K_new perspective: " << std::endl << K_new_p << std::endl;
     std::cout << "K_new longilati: " << std::endl << K_new_l << std::endl;
@@ -85,11 +114,11 @@ int main(void)
     cv::Mat img = cv::imread(imageFilename);
     cv::Mat undistorted_p, undistorted_l, undistorted_f;
 
-    cv::omnidir::undistortImage(img, undistorted_p, K, D, xi, cv::omnidir::RECTIFY_PERSPECTIVE, K_new_p, imageSize, E);
-    cv::omnidir::undistortImage(img, undistorted_l, K, D, xi, cv::omnidir::RECTIFY_LONGLATI, K_new_l, imageSize, E);
+    cv::omnidir::undistortImage(img, undistorted_p, K, D, xi, cv::omnidir::RECTIFY_PERSPECTIVE, K_new_p, imageSize, R);
+    cv::omnidir::undistortImage(img, undistorted_l, K, D, xi, cv::omnidir::RECTIFY_LONGLATI, K_new_l, imageSize, R);
 
     cv::Mat map1, map2;
-    cv::fisheye::initUndistortRectifyMap(Kf, Df, E, K_new_f, imageSize, CV_16SC2, map1, map2 );
+    cv::fisheye::initUndistortRectifyMap(Kf, Df, R, K_new_f, imageSize, CV_16SC2, map1, map2 );
     cv::remap(img, undistorted_f, map1, map2, cv::INTER_LINEAR, cv::BORDER_CONSTANT);
 
     cv::imshow("distorted", img);
@@ -98,7 +127,7 @@ int main(void)
     cv::imshow("undistorted fisheye", undistorted_f);
     cv::waitKey();
 
-    std::string outputFilename = "calibration_results_omni_single_lens_" + std::to_string(scale1) + std::to_string(scale2) + ".yaml";
+    std::string outputFilename = "calibration_results_omni_single_lens_scale0" + std::to_string(scale0) + "_scale1" + std::to_string(scale1) + ((rotAx == "x" || rotAx == "y" || rotAx == "z") ? ("_rotAx" + rotAx + "_rotAng" + std::to_string(rotAng)) : "") + ".yaml";
     cv::FileStorage fs(outputFilename, cv::FileStorage::WRITE);
     fs << "image_width" << imageSize.width;
     fs << "image_height" << imageSize.height;
@@ -108,8 +137,9 @@ int main(void)
     fs << "xi" << xi;
     fs << "rms" << rms;
 
+    fs << "scale0" << scale0;
     fs << "scale1" << scale1;
-    fs << "scale2" << scale2;
+    fs << "R" << R;
     fs << "K_undist_perspective" << K_new_p;
     fs << "K_undist_longilati" << K_new_l;
 
